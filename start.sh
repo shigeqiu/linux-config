0b972327fc334f9245041b5b101389e208f066bb
guosc <guosc_mail@163.com>
2018-06-04 18:27:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo "当前路径`pwd`"
+echo "current path `pwd`"
 
 bash  /data/server/guess-web/springboot.sh stop guess-web.jar guess-web.jar
 
 rm -f /data/server/guess-web/guess-web.jar /data/server/guess-web/out.log
 
-cp guess-web/target/guess-web.jar /data/server/guess-web
+cp ~/jenkins/guess-web.jar /data/server/guess-web
 
 cd /data/server/guess-web
 
-echo "cd后的当前路径`pwd`"
+echo " cd -  change directory"
+echo "current path `pwd`"
 
-bash  springboot.sh start guess-web.jar guess-web.jar
+bash  springboot.sh start "guess-web.jar --server.port=8080" guess-web.jar
